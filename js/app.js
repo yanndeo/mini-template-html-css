4626aab4c9e05abe97b4c6fe0200099d814919bb
Yann Deo <yan2sambou@gmail.com>
2020-05-28 16:18:34 +0000

    project list

--- js/app.js
+++ js/app.js
@@ -12,3 +12,38 @@ $(document).ready(function () {
     });
 
 });
+
+
+
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 4500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        fadeEffect: {
+            crossFade: true
+        },
+
+          keyboard: {
+              enabled: true,
+              onlyInViewport: false,
+          },
+    });
+
+   
+});
+
+
